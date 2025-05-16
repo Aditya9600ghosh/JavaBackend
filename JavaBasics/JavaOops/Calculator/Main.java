c2a2494000d0d17dd19b16505fd4813c2b264ee4
Aditya9600ghosh <ghoshaditya9600@gmail.com>
2025-05-16 12:35:07 +0530

    Java Basic OOPs

---
+++ JavaBasics/JavaOops/Calculator/Main.java
@@ -0,0 +1,17 @@
+package JavaBackend.JavaBasics.JavaOops.Calculator;
+
+class Calculator {
+
+    public int add(int n1, int n2){
+        return n1+n2;
+    }
+}
+
+public class Main{
+    public static void main(String[] args) {
+        int a = 4, b = 5;
+        Calculator obj = new Calculator();
+        int ans = obj.add(a,b);
+        System.out.println(ans);
+    }
+}
---
+++ JavaBasics/JavaOops/DataAreaHeap/Demo.java
@@ -0,0 +1,26 @@
+package JavaBackend.JavaBasics.JavaOops.DataAreaHeap;
+
+
+class Calculator {
+
+    int num = 5; // instance variable
+
+    public int add(int n1, int n2){
+        System.out.println("Accessing instance var num inside add method : " + num);
+        return n1+n2;
+    }
+}
+
+public class Demo {
+    public static void main(String[] args) {
+        int data = 10;
+        System.out.println("Local variable of main method : " + data);
+        Calculator obj1 = new Calculator();
+        System.out.println("Addition of 3 & 4 = " + obj1.add(3,4));
+
+        Calculator obj2 = new Calculator();
+        obj1.num = 12;
+        System.out.println(obj1.num);
+        System.out.println(obj2.num);
+    }
+}
