570e29740b41b93b5a533b80d3be1bd5472cf10f
Aditya9600ghosh <ghoshaditya9600@gmail.com>
2025-05-19 11:26:26 +0530

    String Concepts

---
+++ JavaBasics/StringConcepts/BasicsofStrings/Demo.java
@@ -0,0 +1,18 @@
+package JavaBackend.JavaBasics.StringConcepts.BasicsofStrings;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        // String name = "Aditya";
+
+        // or
+
+        String name = new String("Aditya");
+        System.out.println(name.hashCode());
+        System.out.println("hello " + name);
+
+        System.out.println(name.charAt(2));
+
+        System.out.println(name.concat(" Ghosh"));
+    }
+}
---
+++ JavaBasics/StringConcepts/StringBuffer_concept/Demo.java
@@ -0,0 +1,24 @@
+package JavaBackend.JavaBasics.StringConcepts.StringBuffer_concept;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        // By default StringBuffer has the Buffer size or capacity of 16 bits
+        StringBuffer obj = new StringBuffer();
+        System.out.println(obj.capacity());
+
+        StringBuffer sb = new StringBuffer("Aditya");
+        System.out.println(sb.capacity());  // give capacity of 22 bits
+        System.out.println(sb.length()); // while the length is 6 of Aditya
+        System.out.println(sb.append(" Ghosh")); // here you can change the string data
+        sb.deleteCharAt(3);
+        sb.insert(4, "JavaLang");
+        sb.setLength(30);
+        sb.ensureCapacity(100);
+        System.out.println(sb);
+
+
+        // String name = sb.toString();
+        // System.out.println(name);
+    }
+}
