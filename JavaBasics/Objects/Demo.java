f648a5182170e0dcdeeb5e7834318d04b6e3210a
Aditya9600ghosh <ghoshaditya9600@gmail.com>
2025-07-23 12:05:51 +0530

    concepts of objects

---
+++ JavaBasics/Objects/Demo.java
@@ -0,0 +1,25 @@
+package JavaBackend.JavaBasics.Objects;
+
+class A{
+    public A(){
+        System.out.println("Object created & Constructor Invoked");
+    }
+
+    public void show(){
+        System.out.println("in A's show");
+    }
+}
+
+public class Demo {
+    public static void main(String[] args) {
+
+        // new A();  // anonymous object because it does not have a name
+        new A().show();
+
+
+
+        // A obj;  // so here you are creating a reference variable
+        // obj = new A();    // this new A() is called object creation & assigning the value to obj
+        // obj.show();
+    }
+}
