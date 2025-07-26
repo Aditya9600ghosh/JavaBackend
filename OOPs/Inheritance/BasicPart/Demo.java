b994807860792960c5840504d7e15dabbd42605d
Aditya9600ghosh <ghoshaditya9600@gmail.com>
2025-07-26 16:11:39 +0530

    latest

---
+++ OOPs/Inheritance/BasicPart/Demo.java
@@ -0,0 +1,12 @@
+public class Demo{
+    public static void main(String[] args) {
+        AdvCalc obj = new AdvCalc();
+        int r1 = obj.add(7, 12);
+        int r2 = obj.sub(12, 7);
+        int r3 = obj.multi(5, 4);
+        int r4 = obj.div(4, 2);
+
+        System.out.println("Sum = " + r1 + " " + "Difference = " + r2);
+        System.out.println("Prod = " + r3 + " " + "Div = " + r4);
+    }
+}
