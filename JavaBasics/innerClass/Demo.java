5a1742268bb506039276d3208d9259ce6a85f98c
Aditya9600ghosh <ghoshaditya9600@gmail.com>
2025-08-28 23:30:20 +0530

    InnerClass

--- JavaBasics/innerClass/Demo.java
+++ JavaBasics/innerClass/Demo.java
@@ -1,17 +1,17 @@
-class A{
+class Outer{
     int age;
 
     public void show(){
         System.out.println("in show");
     }
 
-    // class B{
+    // class Inner{
     //     public void config(){
     //         System.out.println("in Config");
     //     }
     // }
 
-    static class B{ // here static can be used only for inner class
+    static class Inner{ // here static can be used only for inner class
         public void config(){
             System.out.println("in Config");
         }
@@ -19,16 +19,16 @@ class A{
 }
 public class Demo {
     public static void main(String[] args) {
-        A obj = new A();
-        obj.show();
+        Outer outerObject = new Outer();
+        outerObject.show();
 
 
-        // if you want to access the method config, you can't create the object of B directly
-        // A.B obj1 = obj.new B();  // since B class is a non static class that's why we need object to use it
+        // if you want to access the method config, you can't create the object of Inner class directly
+        // Outer.Inner innerObject = outerObject.new Inner();  // since Inner class is a non static class that's why we need object to use it
 
 
         // if you make the class B as static then
-        A.B obj1 = new A.B();
-        obj1.config();
+        Outer.Inner innerObject = new Outer.Inner();
+        innerObject.config();
     }
 }
